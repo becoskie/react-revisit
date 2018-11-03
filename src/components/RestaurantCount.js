1a9c71a02f83ddc7275490e0f6d057229bfbc4ad
Ray Becoskie <ray@becoskie.com>
2018-11-03 10:27:18 -0500

    more info in readme, refactored components into stateless

--- src/components/RestaurantCount.js
+++ src/components/RestaurantCount.js
@@ -1,35 +1,32 @@
-import { Component } from "react";
 import "../stylesheets/ui.scss";
 
-export class RestaurantCount extends Component {
-  percentToDecimal(decimal) {
-    return decimal * 100 + "%";
-  }
-  calcGoalProgress(total, goal) {
-    return this.percentToDecimal(total / goal);
-  }
-
-  render() {
-    return (
-      <div className="restaurant-day-count">
-        <div className="total-days">
-          <span>{this.props.total}</span>
-          <span>days</span>
-        </div>
-        <div className="fancy-days">
-          <span>{this.props.fancy}</span>
-          <span>days</span>
-        </div>
-        <div className="regular-days">
-          <span>{this.props.regular}</span>
-          <span>days</span>
-        </div>
-        <div>
-          <span>
-            {this.calcGoalProgress(this.props.total, this.props.goal)}
-          </span>
-        </div>
-      </div>
-    );
-  }
+const percentToDecimal = (decimal) => {
+	return decimal * 100 + "%";
 }
+
+const calcGoalProgress = (total, goal) => {
+	return percentToDecimal(total / goal);
+}
+ 
+
+export const RestaurantCount = ({total, fancy, regular, goal}) => (
+	<div className="restaurant-day-count">
+	<div className="total-days">
+		<span>{total}</span>
+		<span>days</span>
+	</div>
+	<div className="fancy-days">
+		<span>{fancy}</span>
+		<span>days</span>
+	</div>
+	<div className="regular-days">
+		<span>{regular}</span>
+		<span>days</span>
+	</div>
+	<div>
+		<span>
+		{calcGoalProgress(total, goal)}
+		</span>
+	</div>
+	</div>
+)
